52910240f4a6469b21f4b3ae187aa5eed77e8552
Matthew Love <matthewtglove@gmail.com>
2025-04-24 10:33:32 -0500

    Re-added video as React

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,30 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
+  const [intLastReportedTime, setIntLastReportedTime] = useState(0);
+  const videoElement = document.querySelector("video");
+
+  if (videoElement) {
+    videoElement.addEventListener("pause", () => {
+      videoElement.play();
+    });
+
+    videoElement.addEventListener("ratechange", () => {
+      videoElement.playbackRate = 1.0;
+    });
+
+    videoElement.addEventListener("timeupdate", () => {
+      if (
+        videoElement.currentTime < intLastReportedTime ||
+        videoElement.currentTime > intLastReportedTime + 1
+      ) {
+        videoElement.currentTime = intLastReportedTime;
+      }
+      console.log(videoElement.currentTime);
+      setIntLastReportedTime(videoElement.currentTime);
+    });
+  }
+
   return (
     <>
       <div>
@@ -26,6 +50,22 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <head>
+        <title>CSC2400 Extra Credit Project</title>
+      </head>
+      <div>
+        <video
+          height="500"
+          src="http://download.blender.org/peach/bigbuckbunny_movies/big_buck_bunny_1080p_h264.mov"
+          autoPlay
+        >
+          <p>I'm not Catholic, but welcome to Purgatory</p>
+        </video>
+        <p style={{ fontSize: "9px", textAlign: "center" }}>
+          This project was developed by Matthew Love (mtlove42), Rivers Haley
+          (jrhaley42), and Chase Gibbs (cdgibbs42).
+        </p>
+      </div>
     </>
   );
 }
